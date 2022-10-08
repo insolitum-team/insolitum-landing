a820e24cb82efab410363e2c508f01466c4dca03
SidVerson <whatislove354@gmail.com>
2022-10-08 13:19:58 +0300

    minor changes

--- js/main.js
+++ js/main.js
@@ -1,27 +1,6 @@
 
-var chr = -1;
-var txt = 'Development for future.';
-var l = txt.length;
-var p = "_";
-function printtext()
-{
-    chr++;
-    t = document.all["text"].innerHTML;
-    tt = t.substring(0,t.length-p.length);
-    if(txt.charAt(chr)!="<")
-    {
-        document.all["text"].innerHTML= tt + txt.charAt(chr) + p;
-    }
-    else {
-        document.all["text"].innerHTML= tt + txt.substring(chr, chr+4) + p;
-        chr=chr+3;
-    }
-    setTimeout("printtext()", 100);
-}
-function str(num) {
-    var s = "";
-    for(i=0; i<num; i++) {
-        s = s + " ";
-    }
-    return s ;
-}
+var typed = new Typed('#typed', {
+    typeSpeed: 50,
+    stringsElement: '#typed-strings',
+    cursorChar: '_'
+  });
